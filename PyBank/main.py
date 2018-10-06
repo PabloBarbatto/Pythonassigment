8b7d0d66833af61658df2cfc876ff60bb32be608
pablonbarbatto@gmail.com <pablonbarbatto@gmail.com>
2018-10-06 08:44:49 -0400

    pybank done

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,10 @@ prevline = 0
 curline = 0 
 numofchanges = 0
 changebwtmonths = 0
-
+maxchg = 0
+minchg = 0 
+maxdate = str
+mindate = str
 
 csvpath = os.path.join('budget_data.csv')
 with open (csvpath, newline= '') as csvfile:
@@ -16,34 +19,46 @@ with open (csvpath, newline= '') as csvfile:
     for row in csvreader:
         months = months + 1
         profitlosses = int(row[1]) + profitlosses
-        
+        #prevline = curline
+        #curline = int(row[1])
         
         if (months -1) == 0:
             prevline = int(row[1])
-            curline = int(row[1])
-            
- #       elif (months -1) == 1:
-  #          prevline = curline
-  #          curline = int(row[1]) 
-   #         changebwtmonths = (curline - prevline)
-  #          numofchanges =numofchanges + 1
+            curline = int(row[1])     
         else:
             prevline = curline
             curline = int(row[1])
             numofchanges =numofchanges + 1
             changebwtmonths = (((curline) - (prevline)) + changebwtmonths)
-                
-                 
+            if maxchg < (curline - prevline):
+                maxchg = (curline - prevline)
+                maxdate = str(row[0])
+            else:
+                maxchg = maxchg
+            if minchg > (curline - prevline):
+                minchg = (curline - prevline)
+                mindate = str(row[0])
+            else:
+                minchg = minchg     
         
-        
-        print(row)
-        print(prevline)
-        print(curline)
         
 
+avgchangeprolos = round(changebwtmonths/numofchanges, 2)
 
-        
-    avgchangeprolos = round(changebwtmonths/numofchanges, 2)
-    print(months)
-    print(profitlosses)
-    print(avgchangeprolos)
+print("Financial Analysis")
+print("----------------------------")
+print("Total Months: " + str(months))
+print("Total: $"+str(profitlosses))
+print("Average  Change: $"+str(avgchangeprolos))
+print("Greatest Increase in Profits: " + maxdate + " (" +str(maxchg) + ")")
+print("Greatest Decrease in Profits: " + mindate + " (" +str(minchg) + ")")
+
+output_dest = os.path.join('pybank_output.txt')
+with open(output_dest, 'w') as writefile:
+    writefile.write("Financial Analysis\n")
+    writefile.write("----------------------------\n")
+    writefile.write("Total Months: " + str(months)+"\n")
+    writefile.write("Total: $"+str(profitlosses) +"\n")
+    writefile.write("Average  Change: $"+str(avgchangeprolos)+"\n")
+    writefile.write("Greatest Increase in Profits: " + maxdate + " (" +str(maxchg) + ")"+"\n")
+    writefile.write("Greatest Decrease in Profits: " + mindate + " (" +str(minchg) + ")"+"\n")
